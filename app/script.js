d85db91f84b52bbf1c7998799bd386afc219be3f
Michael Sexton <ms3648@cornell.edu>
2024-01-31 15:31:40 -0500

    CS Degree audit working

--- app/script.js
+++ app/script.js
@@ -4,12 +4,19 @@ var coursesTaken = [];
 function loadCoursesTaken() {
   // Retrieve the JSON string from local storage
   var coursesTakenJSON = localStorage.getItem("coursesTaken");
+  var programJSON = localStorage.getItem("program");
 
   if (coursesTakenJSON !== null) {
     coursesTaken = JSON.parse(coursesTakenJSON);
   }
+
+  if (programJSON !== null) {
+    program = JSON.parse(programJSON);
+  }
+  updateDegreeProgramDropdown();
   updateCourseListDisplay();
   updateRemoveCourseDropdown();
+  updateDegreeProgram();
 }
 
 // Add an event listener to trigger the loadCoursesTaken function when the page is loaded
@@ -17,43 +24,76 @@ document.addEventListener("DOMContentLoaded", function () {
   loadCoursesTaken();
 });
 
+function updateDegreeProgramDropdown() {
+  var degreeProgramDropdown = document.getElementById("degreeProgram");
+  degreeProgramDropdown.value = program;
+}
+
+function updateDegreeProgram() {
+  var degreeProgramDisplay = document.getElementById("degreeProgramDisplay");
+  degreeProgramDisplay.innerHTML = "Program: " + program;
+}
+
+function saveDegreeProgram() {
+  // Save program to local storage
+  var programJSON = JSON.stringify(program);
+  localStorage.setItem("program", programJSON);
+}
+
 function saveCoursesTaken() {
   // Save coursesTaken to local storage
   var coursesTakenJSON = JSON.stringify(coursesTaken);
   localStorage.setItem("coursesTaken", coursesTakenJSON);
+
+  var programJSON = JSON.stringify(program);
+  localStorage.setItem("program", coursesTakenJSON);
 }
 
 function addEntry() {
   var courseSubject = document.getElementById("courseSubject").value;
   var classCode = document.getElementById("classCode").value;
+  var className = document.getElementById("className").value;
   var credits = document.getElementById("credits").value;
   var semester = document.getElementById("semester").value;
 
-  var course = {
-    subject: courseSubject,
-    courseCode: classCode,
-    credits: credits,
-    semester: semester,
-  };
+  //validate there is input before adding class
+  if (
+    courseSubject === "" ||
+    classCode === "" ||
+    credits === "" ||
+    semester === ""
+  ) {
+    alert("Please complete all fields");
+  } else {
+    var course = {
+      subject: courseSubject,
+      courseCode: classCode,
+      courseName: className,
+      credits: credits,
+      semester: semester,
+    };
 
-  coursesTaken.push(course);
+    coursesTaken.push(course);
 
-  var entry =
-    "Subject: " +
-    courseSubject +
-    ", Course: " +
-    classCode +
-    ", Credits: " +
-    credits +
-    ", Semester: " +
-    semester;
-  var entryDiv = document.getElementById("entries");
-  entryDiv.innerHTML += "<p>" + entry + "</p>";
+    var entry =
+      "Subject: " +
+      courseSubject +
+      ", Course: " +
+      classCode +
+      ", Course Name: " +
+      className +
+      ", Credits: " +
+      credits +
+      ", Semester: " +
+      semester;
+    var entryDiv = document.getElementById("entries");
+    entryDiv.innerHTML += "<p>" + entry + "</p>";
 
-  updateCourseListDisplay();
-  updateRemoveCourseDropdown();
+    updateCourseListDisplay();
+    updateRemoveCourseDropdown();
 
-  saveCoursesTaken();
+    saveCoursesTaken();
+  }
 }
 
 function removeSelectedCourse() {
@@ -96,6 +136,8 @@ function updateCourseListDisplay() {
       course.courseCode +
       ", Credits: " +
       course.credits +
+      ", Course Name: " +
+      course.courseName +
       ", Semester: " +
       course.semester;
     entryDiv.innerHTML += "<p>" + entry + "</p>";
@@ -117,73 +159,72 @@ function updateRemoveCourseDropdown() {
     );
   }
 }
-function auditDegree() {
-  var degreeProgram = document.getElementById("degreeProgram").value;
-  console.log(degreeProgram);
-
-  if (degreeProgram === "CM") {
-    CM_auditDegree();
-  } else if (degreeProgram === "HT") {
-    HT_auditDegree();
-  } else if (degreeProgram === "UT") {
-    UT_auditDegree();
-  } else if (degreeProgram === "MBA") {
-    MBA_auditDegree();
-  } else if (degreeProgram === "CS") {
-    CS_auditDegree();
-  } else if (degreeProgram === "ECE") {
-    ECE_auditDegree();
-  } else if (degreeProgram === "LLM") {
-    LLM_auditDegree();
-  } else if (degreeProgram === "DT") {
-    DT_auditDegree();
-  } else if (degreeProgram === "ORIE") {
-    ORIE_auditDegree();
-  } else {
-    alert("Please select a degree program");
-  }
-}
 
 function CS_auditDegree() {
-  var degreeProgram = document.getElementById("degreeProgram").value;
-  var degreeProgramDisplay = document.getElementById("degreeProgramDisplay");
-  var auditResults = document.getElementById("auditResults");
-
-  degreeProgramDisplay.innerHTML = "Program: " + degreeProgram;
-
-  var countedCourses = [];
-  let csCredits = 0;
-  let additionalTechCredits = 0;
-  let techCredits = 0;
-  let electiveCredits = 0;
-  let csCoursesTaken = [];
+  var csCredits = 0;
+  var additionalTechCredits = 0;
+  var techCredits = 0;
+  var electiveCredits = 0;
+  var courseGroupings = "";
 
   // Iterate through coursesTaken
-  courses.forEach((course) => {
-    course.push(course);
-    // Check if it's a CS course
-    if (course.subject === "CS") {
-      // Check if it's not already counted
-      if (!csCoursesTaken.includes(course.courseCode)) {
-        csCredits += course.credits;
-        csCoursesTaken.push(course.courseCode);
-      }
-    }
-    // Check for additional tech credits
-    else if (
+  coursesTaken.forEach((course) => {
+    if (course.subject === "CS" && csCredits < 15) {
+      courseGroupings +=
+        "CS: " +
+        course.subject +
+        " " +
+        course.courseCode +
+        " " +
+        course.courseName +
+        " " +
+        course.credits +
+        " credits <br>";
+      csCredits += Number(course.credits);
+    } else if (
       ["CS", "ECE", "INFO", "ORIE"].includes(course.subject) &&
       course.courseCode >= 5000 &&
       additionalTechCredits < 3
     ) {
-      additionalTechCredits += course.credits;
+      courseGroupings +=
+        "Tech Elective: " +
+        course.subject +
+        " " +
+        course.courseCode +
+        " " +
+        course.courseName +
+        " " +
+        course.credits +
+        " credits <br>";
+      additionalTechCredits += Number(course.credits);
+    } else if (course.subject === "TECH" && techCredits < 8) {
+      courseGroupings +=
+        "Tech (Studio): " +
+        course.subject +
+        " " +
+        course.courseCode +
+        " " +
+        course.courseName +
+        " " +
+        course.credits +
+        " credits <br>";
+
+      techCredits += Number(course.credits);
     }
-    // Check for TECH credits
-    else if (course.subject === "TECH") {
-      techCredits += course.credits;
-    }
-    // Any other courses are counted as electives
+    // Any remaining courses are counted as electives
     else {
-      electiveCredits += course.credits;
+      courseGroupings +=
+        "Elective: " +
+        course.subject +
+        " " +
+        course.courseCode +
+        " " +
+        course.courseName +
+        " " +
+        course.credits +
+        " credits <br>";
+
+      electiveCredits += Number(course.credits);
     }
   });
 
@@ -206,23 +247,44 @@ function CS_auditDegree() {
     meetsTechCredits &&
     meetsElectiveCredits;
 
+  var auditResults = document.getElementById("auditResults");
+
   auditResults.innerHTML =
     "Audit Results: " +
-    "Meets CS Credits: " +
+    "<strong>Meets CS Credits:</strong> " +
+    csCredits +
+    "/" +
+    csCreditsRequired +
+    " " +
     meetsCSCredits +
-    ", Meets Additional Tech Credits: " +
+    ", <strong>Meets Additional Tech Credits:</strong> " +
+    additionalTechCredits +
+    "/" +
+    additionalTechCreditsRequired +
+    " " +
     meetsAdditionalTechCredits +
-    ", Meets Tech Credits: " +
+    ", <strong>Meets Tech Credits:</strong> " +
+    techCredits +
+    "/" +
+    techCreditsRequired +
+    " " +
     meetsTechCredits +
-    ", Meets Elective Credits: " +
+    ", <strong>Meets Elective Credits:</strong> " +
+    electiveCredits +
+    "/" +
+    electiveCreditsRequired +
+    " " +
     meetsElectiveCredits;
 
   if (allRequirementsMet) {
     auditResults.innerHTML += " - <strong>Passed</strong>";
   } else {
     auditResults.innerHTML +=
-      " - <br> <strong>Reassess your schedule, you are not on track to graduate</strong>";
+      " <br> <strong>Reassess your schedule, you are not on track to graduate</strong>";
   }
+
+  var courseGroupingsDisplay = document.getElementById("courseGroupings");
+  courseGroupingsDisplay.innerHTML = courseGroupings;
 }
 
 function CM_auditDegree() {
@@ -241,10 +303,6 @@ function MBA_auditDegree() {
   alert("not yet implemented");
 }
 
-function CS_auditDegree() {
-  alert("not yet implemented");
-}
-
 function ECE_auditDegree() {
   alert("not yet implemented");
 }
@@ -259,4 +317,38 @@ function DT_auditDegree() {
 
 function ORIE_auditDegree() {
   alert("not yet implemented");
+}
+
+function auditDegree() {
+  program = document.getElementById("degreeProgram").value;
+  if (program === "") {
+    alert("Please select a degree program");
+  } else if (coursesTaken.length === 0) {
+    alert("Please enter a course");
+  } else {
+    saveDegreeProgram();
+    updateDegreeProgram();
+
+    if (program === "CM") {
+      CM_auditDegree();
+    } else if (program === "HT") {
+      HT_auditDegree();
+    } else if (program === "UT") {
+      UT_auditDegree();
+    } else if (program === "MBA") {
+      MBA_auditDegree();
+    } else if (program === "CS") {
+      CS_auditDegree();
+    } else if (program === "ECE") {
+      ECE_auditDegree();
+    } else if (program === "LLM") {
+      LLM_auditDegree();
+    } else if (program === "DT") {
+      DT_auditDegree();
+    } else if (program === "ORIE") {
+      ORIE_auditDegree();
+    } else {
+      alert("Please select a degree program");
+    }
+  }
 }
